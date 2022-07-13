e5b39acc66ce70a3789ec9614f3c4182ff133a6e
Ben Schultz <bschultz1990@gmail.com>
2022-07-12 21:43:10 -0500

    Added weight totaler functions.

--- commercial_invoice.js
+++ commercial_invoice.js
@@ -1,222 +1,138 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableElement/insertRow
 // https://snippet-generator.app/
 
-const date = document.getElementById("form-date")
-const shipDate = document.getElementById("ship-date")
-const sigDate = document.getElementById("sig-date")
-const sigFile = document.getElementById("sig-file")
-const gridWrapper = document.getElementById("grid-wrapper")
-let formQty = document.querySelectorAll(".form-qty")
-const formDesc = document.querySelectorAll(".form-desc")
-let formWt = document.querySelectorAll(".form-weight")
-const totalWt = document.getElementById("totalweight")
-let formVal = document.querySelectorAll(".form-value")
-const btnPlus = document.getElementById("buttonPlus")
-const btnMinus = document.getElementById("buttonMinus")
-const invisForm = document.querySelectorAll(".invis-form")
+let date = document.getElementById("form-date");
+let shipDate = document.getElementById("ship-date");
+let sigDate = document.getElementById("sig-date");
+let sigFile = document.getElementById("sig-file");
+let gridWrapper = document.getElementById("grid-wrapper");
+let formQty = document.getElementById("form-qty-1");
+let formDesc = document.querySelectorAll(".form-desc");
+let formWeight = document.getElementById("form-weight-1");
+let totalWt = document.getElementById("totalweight");
+let formValue = document.getElementById("form-value-1");
+let btnPlus = document.getElementById("buttonPlus");
+let btnMinus = document.getElementById("buttonMinus");
+let invisForm = document.querySelectorAll(".invis-form");
 
-let rowCount = 1
+let rowCount = 1;
+
+formQty.addEventListener("change", totalWeight); // add event listener to first qty
+formWeight.addEventListener("change", totalWeight); // add event listener to first weight
+// TODO: newQty.addEventListener("change", totalValue);
 
 date.addEventListener("change", function (e) {
-	e.preventDefault();
-	// update dates
-	shipDate.innerText = date.value
-	sigDate.innerText = date.value
+    e.preventDefault();
+    // update dates
+    shipDate.innerText = date.value;
+    sigDate.innerText = date.value;
 });
 
-sigFile.addEventListener("change", function (e){
-	e.preventDefault();
-	const sigImg = document.querySelector('#sig-img')
-	URL.revokeObjectURL(e.target.files[0].name)
-	sigImg.src = URL.createObjectURL(e.target.files[0]); // Assign source to image
-	console.dir(sigImg.src)
+sigFile.addEventListener("change", function (e) {
+    e.preventDefault();
+    const sigImg = document.querySelector("#sig-img");
+    URL.revokeObjectURL(e.target.files[0].name);
+    sigImg.src = URL.createObjectURL(e.target.files[0]); // Assign source to image
+    console.dir(sigImg.src);
 });
-
 
 btnPlus.addEventListener("click", function (e) {
-	e.preventDefault();
+    e.preventDefault();
 
-	rowCount++
-	
-	const newQty = document.createElement("input")
-	newQty.type = "text"
-	newQty.id = `form-qty-${rowCount}`
-	newQty.classList.add("invis-form")
-	newQty.classList.add("form-qty")
-	newQty.classList.add("col-qty")
-	newQty.name = `form-qty-${rowCount}`
-	newQty.placeholder = "Qty."
-	// console.dir(gridWrapper)
-	gridWrapper.appendChild(newQty);
+    rowCount++;
 
-	const newDesc = document.createElement("input")
-	newDesc.type = "text"
-	newDesc.id = `form-desc-${rowCount}`
-	newDesc.classList.add("invis-form")
-	newDesc.classList.add("form-desc")
-	newDesc.name = `form-desc-${rowCount}`
-	newDesc.placeholder = "Description"
-	gridWrapper.appendChild(newDesc); // 1=desc col index
+    const newQty = document.createElement("input");
+    newQty.type = "text";
+    newQty.id = `form-qty-${rowCount}`;
+    newQty.classList.add("invis-form");
+    newQty.classList.add("form-qty");
+    newQty.classList.add("col-qty");
+    newQty.name = `form-qty-${rowCount}`;
+    newQty.placeholder = "Qty.";
+    newQty.addEventListener("change", totalWeight);
+    // TODO: newQty.addEventListener("change", totalValue);
+    gridWrapper.appendChild(newQty);
 
-	const newWt = document.createElement("input")
-	newWt.type = "text"
-	newWt.id = `form-weight-${rowCount}`
-	newWt.classList.add("invis-form")
-	newWt.classList.add("form-weight")
-	newWt.classList.add("col-wt")
-	newWt.name = `form-weight-${rowCount}`
-	newWt.placeholder = "Wt."
-	gridWrapper.appendChild(newWt);
+    const newDesc = document.createElement("input");
+    newDesc.type = "text";
+    newDesc.id = `form-desc-${rowCount}`;
+    newDesc.classList.add("invis-form");
+    newDesc.classList.add("form-desc");
+    newDesc.name = `form-desc-${rowCount}`;
+    newDesc.placeholder = "Description";
+    gridWrapper.appendChild(newDesc); // 1=desc col index
 
-	const newVal = document.createElement("input")
-	newVal.type = "text"
-	newVal.id = `form-value-${rowCount}`
-	newVal.classList.add("invis-form")
-	newVal.classList.add("form-value")
-	newVal.classList.add("col-val")
-	newVal.name = `form-value-${rowCount}`
-	newVal.placeholder = "Value $"
-	gridWrapper.appendChild(newVal)
-})
+    const newWt = document.createElement("input");
+    newWt.type = "text";
+    newWt.id = `form-weight-${rowCount}`;
+    newWt.classList.add("invis-form");
+    newWt.classList.add("form-weight");
+    newWt.classList.add("col-wt");
+    newWt.name = `form-weight-${rowCount}`;
+    newWt.placeholder = "Wt.";
+    newWt.addEventListener("change", totalWeight);
+    gridWrapper.appendChild(newWt);
 
+    const newVal = document.createElement("input");
+    newVal.type = "text";
+    newVal.id = `form-value-${rowCount}`;
+    newVal.classList.add("invis-form");
+    newVal.classList.add("form-value");
+    newVal.classList.add("col-val");
+    newVal.name = `form-value-${rowCount}`;
+    newVal.placeholder = "Value $";
+    // TODO: newVal.addEventListener("change", totalValue);
+    gridWrapper.appendChild(newVal);
+});
 
-btnMinus.addEventListener("click", function (e){//{{{
-	e.preventDefault();
+btnMinus.addEventListener("click", function (e) {
+    //{{{
+    e.preventDefault();
 
-	const matchTest = gridWrapper.lastElementChild.id
-	if (new RegExp('form-value-1').test(matchTest) === false) {
-		console.log("No match. Deleting row...");
-		for (let i = 0; i < 4; i++) {
-			gridWrapper.lastElementChild.remove();
-		}
-		rowCount--
-	} else {
-		console.log(`Found first row. Not deleting.`);
-	}
-});//}}}
+    const matchTest = gridWrapper.lastElementChild.id;
+    if (new RegExp("form-value-1").test(matchTest) === false) {
+        console.log("No match. Deleting row...");
+        for (let i = 0; i < 4; i++) {
+            gridWrapper.lastElementChild.remove();
+        }
+        rowCount--;
+    } else {
+        console.log(`Found first row. Not deleting.`);
+    }
+}); //}}}
 
+function colProduct(idPrefix1, idPrefix2) {
+    let rowTotals = []; // empty array
 
-function addColumn(selector){
-	let wtCol = []
-	colSelect = document.querySelectorAll(selector)
+    for (let i = 1; i <= rowCount; i++) {
+        rowTotals.push(multCells(i, idPrefix1, idPrefix2));
+    }
 
-	for (let i of colSelect) {
-		wtCol.push(parseInt(i.value));
-	}
-	let subTotal = 0;
-	let colTotal = wtCol.reduce((x, y) => x+y, subTotal);
-	console.dir(`Column total is:`);
-	return colTotal;
+    let subTotal = 0;
+    return rowTotals.reduce((x, y) => x + y, subTotal);
 }
 
-
-function listenAdder(element, listener, fn, fnArg) {
-	for (i of element) {
-		i.addEventListener(listener, fn(fnArg))
-	}
-	return 
+function totalWeight() {
+    totalWt.innerText = colProduct("form-qty", "form-weight");
 }
 
+function multCells(rowNum, idPrefix1, idPrefix2) {
+    // Append rowCount to our getElementById to get a selector
+    let prodArray = [];
 
+    let selector1 = document.getElementById(idPrefix1.concat("-", rowNum));
+    prodArray.push(parseFloat(selector1.value));
 
+    let selector2 = document.getElementById(idPrefix2.concat("-", rowNum));
+    prodArray.push(parseFloat(selector2.value));
 
+    let subTotal = 1;
+    return prodArray.reduce((x, y) => x * y, subTotal);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function addWeights() {
-//     // iterate over an array. Get all the weights (index[3] of each item.)
-//     let weights = [];
-//     for (let line of units) {
-//         weights.push(line[3]); // push weight column into its own array
+// function listenAdder(element, listener, fn, fnArg) {
+//     for (i of element) {
+//         i.addEventListener(listener, fn(fnArg));
 //     }
-//     // total the weights up
-//     return (totalWeight = weights.reduce((total, i) => total + i));
+//     return;
 // }
-
-// // add total value:
-// function addValues() {
-//     let value = [];
-//     for (let line of units) {
-//         value.push(line[4]); // push the values onto their own array
-//     }
-//     return (grandValue = value.reduce((total, i) => total + i));
-// }
-
-// addWeights();
-
-// // assign total weight;
-// let shipTotalWeight = document.getElementById("totalweight");
-// shipTotalWeight.innerText = totalWeight + " lbs.";
-
-// addValues();
-
-// // assign total value;
-// let shipTotalValue = document.getElementById("totalValue");
-// shipTotalValue.innerText = "$" + grandValue;
-
-// let formPkgCount = document.getElementById("form-pkg-count");
-// let orderTable = document.getElementById("orderTable");
-// let formOrder = document.getElementById("form-order");
-// let formDate = document.getElementById("form-date");
-// let shippingTable = document.getElementById("shippingTable");
-// let sigDate = document.getElementById("date");
-
-// let toFromTable = document.getElementById("toFromTable");
-// let formCstName = document.getElementById("form-cst-name");
-// let formCstAddress = document.getElementById("form-cst-address");
-// let shiptotable = document.getElementById("shiptotable");
-
-// // Submit Button Magic
-// const formSubmit = document.getElementById("form-submit");
-// formSubmit.addEventListener("submit", function (e)) {
-//     e.preventDefault(); // Don't refresh the page.
-
-//     orderTable.rows[0].cells[1].innerText = formOrder.value;
-
-//     // Assign dates
-//     orderTable.rows[1].cells[1].innerText = formDate.value;
-//     shippingTable.rows[1].cells[0].innerText = formDate.value;
-//     sigDate.innerText = formDate.value;
-
-//     // Customer name
-//     toFromTable.rows[1].cells[1].innerText = formCstName.value;
-
-//     // Customer address
-//     toFromTable.rows[2].cells[1].innerText = formCstAddress.value;
-
-//     // Package count
-//     shiptotable.rows[3].cells[1].innerText = formPkgCount.valueAsNumber;
-
-//     // Item Details
